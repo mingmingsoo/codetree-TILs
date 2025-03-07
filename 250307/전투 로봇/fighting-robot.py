af2eb3b470fe3adc688b0d1375bee932a5f00fbb
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-07 09:33:00 +0900

    Title: 전투 로봇, Time: 52ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250307/전투 로봇/fighting-robot.py
+++ 250307/전투 로봇/fighting-robot.py
@@ -24,16 +24,16 @@ from collections import deque
 n = int(input())
 grid = [list(map(int, input().split())) for i in range(n)]
 
-r,c,level,eat = -1,-1,2,0
+r, c, level, eat = -1, -1, 2, 0
 for i in range(n):
     for j in range(n):
         if grid[i][j] == 9:
             grid[i][j] = 0
-            r,c = i,j
+            r, c = i, j
 
 time = 0
-row = [-1,1,0,0]
-col = [0,0,1,-1]
+row = [-1, 1, 0, 0]
+col = [0, 0, 1, -1]
 while True:
     # 자기보다 레벨 높은애들은 지나갈 수 없음
     # 자기보다 레벨 낮은애들 없앨 수 있음
@@ -42,34 +42,35 @@ while True:
 
     # 아 거리 계산하는게 쉽지 않네...
 
-    q = deque([(r,c,0)])
-    visited = [[False]* n for i in range(n)]
+    q = deque([(r, c, 0)])
+    visited = [[False] * n for i in range(n)]
     visited[r][c] = True
     while q:
-        r,c,dist = q.popleft()
+        r, c, dist = q.popleft()
 
         for k in range(4):
-            nr = r+row[k]
-            nc = c+col[k]
-            if not(0<=nr<n and 0<=nc<n) or visited[nr][nc]:
+            nr = r + row[k]
+            nc = c + col[k]
+            if not (0 <= nr < n and 0 <= nc < n) or visited[nr][nc]:
                 continue
             if grid[nr][nc] == 0 or grid[nr][nc] == level:
                 visited[nr][nc] = True
-                q.append((nr,nc,dist+1))
-            elif grid[nr][nc]<level:
+                q.append((nr, nc, dist + 1))
+            elif grid[nr][nc] < level:
                 visited[nr][nc] = True
-                heapq.heappush(target_q, (dist+1, nr,nc))
+                heapq.heappush(target_q, (dist + 1, nr, nc))
     if not target_q:
         break
-    dist,target_r,target_c = heapq.heappop(target_q)
-    grid[target_r][target_c] = 0 # 먹는다
-    eat +=1
+    print(target_q)
+    dist, target_r, target_c = heapq.heappop(target_q)
+    print((target_r,target_c),dist)
+    grid[target_r][target_c] = 0  # 먹는다
+    eat += 1
     r = target_r
     c = target_c
     time += dist
     if eat == level:
-        level+=1
-
+        level += 1
+        eat = 0
 
 print(time)
-
