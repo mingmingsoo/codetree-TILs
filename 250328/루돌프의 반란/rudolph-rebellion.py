c5345e9be7c9155ec22550a3cc46019b8705df99
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-28 17:29:57 +0900

    Title: 루돌프의 반란, Time: 89ms, Memory: 19MB, Status: Passed - Codetree

--- 250328/루돌프의 반란/rudolph-rebellion.py
+++ 250328/루돌프의 반란/rudolph-rebellion.py
@@ -1,7 +1,64 @@
 '''
-코드리팩토링: 겹치는 로직 함수화
+코드리팩토링 불필요한 abs 제거, sort 다르게
 1회 틀: sr, sr 이러고 있음
 2회 틀: 루돌프 충돌 수정해놓고 산타 충돌은 수정안함 미쳤어?
+
+문제 설명
+    1. 루돌프 이동 - 8방 우선순위 거리(^2) 주의, r 큰거 , c 큰거
+    2. 산타 순차 이동 - 4방 우선순위 상우하좌
+구상
+    상호작용이 좀 어려운데
+    기사단처럼 q로 써서 움직여야 하는애들 move_lst에 담고
+    옮겨주면서 격자 밖인 애들은 없애줌 걍
+    아 근데 산타이동 할때 순차라
+    산타 1차원 리스트로 관리해주자.
+
+입력
+맵 크기 N, 턴 수 time, 산타 수 santa_num, 루돌푸 힘 ru_p,  산타 힘 s_p
+루돌푸 위치
+산타 위치
+
+필요한 변수
+    - 루돌프 좌표 r,c,d 맵에 따로 표시 안한다
+    - 산타 맵 2차원 배열 grid 넘버링만 해준다
+        산타 1차원 배열
+        방향, 기절시간 담아준다
+        방향과 기절시간 초기값은 -1
+        근데 죽은애는 0으로 해주자.
+    - score 산타 수 + 1만큼 0번쨰에 빈 0 넣어주기
+필요한 함수
+    ru_move() : 루돌프 이동
+        if merge() : 충돌 낫냐?
+            # 여기서 기절시간 기록
+            santa_jump() : 났으면 와라락 이동
+    santa_move() : 산타 순차 이동
+        if 기절시간 <= time -1 : continue 요롷게  해서 넘어가.
+        if merge() : 충돌 낫냐?
+            # 여기서 기절시간 기록
+            santa_jump() : 났으면 와라락 이동
+    exit() : 전부 죽었나 검사
+    scoring(): 살아있는 애들 점수 추가
+
+잘 밀려서 쫓겨나는지
+3 1 2 1 10
+1 1
+1 2 2
+2 3 3
+
+잘 밀려서 안 쫓겨나는지
+4 1 2 1 10
+1 1
+1 2 2
+2 3 3
+
+와라락 밀리는지
+5 1 4 1 10
+1 1
+1 2 2
+2 3 3
+3 4 4
+4 5 5
+
 '''
 from collections import deque
 
@@ -26,12 +83,10 @@ def ru_move():
         if santa == 0:  # 죽은 애 넘어가
             continue
         i, j = santa[0], santa[1]
-        close_santa.append(((i - deer_r) ** 2 + (j - deer_c) ** 2, -i, -j))
+        close_santa.append(((i - deer_r) ** 2 + (j - deer_c) ** 2, i, j))
 
-    close_santa.sort()
+    close_santa.sort(key=lambda x: (x[0], -x[1], -x[2]))
     dist, sr, sc = close_santa[0]
-    sr *= -1
-    sc *= -1
 
     cur = (deer_r - sr) ** 2 + (deer_c - sc) ** 2
     ori_r, ori_c = deer_r, deer_c
@@ -41,7 +96,7 @@ def ru_move():
         nc = ori_c + col[k]
         if not (0 <= nr < n and 0 <= nc < n):
             continue
-        next = (nr - sr) ** 2 + abs(nc - sc) ** 2
+        next = (nr - sr) ** 2 + (nc - sc) ** 2
         if next < cur:
             cur, deer_r, deer_c, d = next, nr, nc, k
 
@@ -112,7 +167,7 @@ def santa_move(t):
             nsc = ori_sc + col[k]
             if not (0 <= nsr < n and 0 <= nsc < n) or grid[nsr][nsc]:
                 continue
-            next = (nsr - deer_r) ** 2 + abs(nsc - deer_c) ** 2
+            next = (nsr - deer_r) ** 2 + (nsc - deer_c) ** 2
             if next < cur:
                 cur, sr, sc, sd = next, nsr, nsc, k
         santa_lst[idx][0] = sr
