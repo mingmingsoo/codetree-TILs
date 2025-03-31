f2cb495aa3e45add3873b287972fad5a5c4b72b1
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-31 16:42:41 +0900

    Title: 마법의 숲 탐색, Time: 300ms, Memory: 23MB, Status: Passed - Codetree

--- 250331/마법의 숲 탐색/magical-forest-exploration.py
+++ 250331/마법의 숲 탐색/magical-forest-exploration.py
@@ -1,35 +1,5 @@
 '''
-틀린이유: 왼/오가 if elif가 아니라 if, if 여야함 ㅠㅠ
-        전에 코드는 왼쪽으로만 가는 코드...
-문제설명
-    1. 정령은 위에서 내려옴
-        중앙, 위, 아래, 왼, 오, 방향 관리
-    2.  while True:
-            남쪽으로 쭉
-            오른쪽/왼쪽 한칸
-                오/왼 못가면 break
-            break 조건은?
-    3. 정령 이동
-            골렘을 넘버링.
-            출구를 -1로 기록
-            내 넘버는 어디든 갈 수 있고
-            출구로는 어디로든지 갈 수 있음
-입력
-    맵 크기 n,m, 정령수 player_num
-    골렘 출발 열, 방향
-    0 1 2 3
-    북동남서
-출력
-    매 턴마다 정령 위치 합
-필요한 변수
-    idx, r,c,d: 골렘 넘버, 중앙, 방향
-    grid: 골렘 2차원 배열 기록(출구 -1)
-필요한 함수
-    down()
-    left()
-    right()
-    bfs()
-    clear()
+실험쓰
 '''
 # ------------------------- 입력 -------------------------
 from collections import deque
@@ -39,6 +9,7 @@ n += 3  # 위로 3개 패딩
 grid = [[0] * m for i in range(n)]
 row = [-1, 0, 1, 0]
 col = [0, 1, 0, -1]
+
 
 # ------------------------- 함수 -------------------------
 def down():
@@ -80,6 +51,7 @@ def bfs():
                 visited[nr][nc] = True
                 q.append((nr, nc, abs(grid[nr][nc])))  # 탈출구까지 겹칠 수 있어서..
 
+
 # ------------------------- 메인 -------------------------
 ans = 0
 for p in range(player_num):
@@ -87,28 +59,24 @@ for p in range(player_num):
     c -= 1
     r = 0  # 맨 처음 중앙은 격자밖
     while True:
-        change = False
         down()
-
         # 위 r-1, c -> r-1, c-1
         # 왼 r, c-1 -> r, c-2
         # 밑 r+1, c -> r+1, c-1
-        if c - 1 > 0 and grid[r - 1][c - 1] == grid[r][c - 2] == grid[r + 1][c - 1] == 0:
-            if r + 1 < n - 1 and grid[r + 1][c - 2] == grid[r + 2][c - 1] == 0:
-                c -= 1
-                r += 1
-                d = (d - 1) % 4
-                change = True
+        if c - 1 > 0 and r + 1 < n - 1 and grid[r - 1][c - 1] == grid[r][c - 2] == grid[r + 1][c - 1] == grid[r + 1][
+            c - 2] == grid[r + 2][c - 1] == 0:
+            c -= 1
+            r += 1
+            d = (d - 1) % 4
         # 위 r-1, c -> r-1, c+1
         # 오 r, c+1 -> r, c+2
         # 밑 r+1, c -> r+1, c+1
-        if c + 1 < m - 1 and grid[r - 1][c + 1] == grid[r][c + 2] == grid[r + 1][c + 1] == 0:
-            if r + 1 < n - 1 and grid[r + 1][c + 2] == grid[r + 2][c + 1] == 0:
-                r += 1
-                c += 1
-                d = (d + 1) % 4
-                change = True
-        if not change:  # 못움직여요!
+        elif c + 1 < m - 1 and r + 1 < n - 1 and grid[r - 1][c + 1] == grid[r][c + 2] == grid[r + 1][c + 1] == \
+                grid[r + 1][c + 2] == grid[r + 2][c + 1] == 0:
+            r += 1
+            c += 1
+            d = (d + 1) % 4
+        else:
             break
 
     grid[r][c] = grid[r - 1][c] = grid[r + 1][c] = grid[r][c - 1] = grid[r][c + 1] = p + 1  # 정령 표시
