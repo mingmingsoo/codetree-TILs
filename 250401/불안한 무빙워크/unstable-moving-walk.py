d4343a1c923ffec1c2c2af18b815169a07d877cd
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-04-01 21:49:50 +0900

    Title: 불안한 무빙워크, Time: 88ms, Memory: 18MB, Status: Passed - Codetree

--- 250401/불안한 무빙워크/unstable-moving-walk.py
+++ 250401/불안한 무빙워크/unstable-moving-walk.py
@@ -20,24 +20,25 @@ while True:
     # 1. 무빙워크 한 칸 회전
     q.rotate(1)
     people.insert(0, people.pop())
-    if people[n-1]:
-        people[n-1] = 0
+    if people[n - 1]:
+        people[n - 1] = 0
 
     # 2. 쩜푸
     for i in range(n - 2, -1, -1):
-        if people[i] and not people[i + 1] and q[i + 1]:
+        if people[i] and not people[i + 1] and q[i + 1] > 0:
             people[i], people[i + 1] = people[i + 1], people[i]
             q[i + 1] -= 1
-    if people[n-1]:
-        people[n-1] = 0
+    if people[n - 1]:
+        people[n - 1] = 0
     # 3. 올려!
-    if not people[0]:
+    if not people[0] and q[0] > 0:
         people[0] = 1
         q[0] -= 1
-    if people[n-1]:
-        people[n-1] = 0
+    if people[n - 1]:
+        people[n - 1] = 0
     # 4. 검증
     if end():
         ans = time
         break
+
 print(time)
