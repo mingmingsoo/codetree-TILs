bc48ea0c1a81b60727d2a5a9a543cd986ef8b082
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-04-01 11:21:57 +0900

    Title: 미지의 공간 탈출, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 250401/미지의 공간 탈출/escape-unknown-space.py
+++ 250401/미지의 공간 탈출/escape-unknown-space.py
@@ -175,8 +175,84 @@ bfs2 : 2차원
 7 7 1 100
 7 7 1 100
 '''
+# --------------------------------- 입력, 좌표찾기 ---------------------------------
+
+from collections import deque
+
+n, m, time_attack_num = map(int, input().split())
+grid = [list(map(int, input().split())) for i in range(n)]
+cube = []
+for _ in range(5):
+    tmp = [list(map(int, input().split())) for i in range(m)]
+    cube.append(tmp)
+
+row = [0, 0, 1, -1]
+col = [1, -1, 0, 0]
+
+sh3, sr3, sc3 = -1, -1, -1  # 3차원 시작점
+eh3, er3, ec3 = -1, -1, -1  # 3차원 탈출구
+sr2, sc2 = -1, -1,  # 2차원 시작점
+er2, ec2 = -1, -1  # 2차원 탈출구
 
 
+def find():  # 3 시작 위치 찾기
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == 3:
+                return i, j
+startx, starty = find()
+
+for i in range(n):  # 2차원 목적지 찾기
+    for j in range(n):
+        if grid[i][j] == 4:
+            er2, ec2 = i, j
+
+for h in range(5):  # 3차원 시작점 찾기
+    for i in range(m):
+        for j in range(m):
+            if cube[h][i][j] == 2:
+                sh3, sr3, sc3 = h, i, j
+
+for i in range(n):  # 2차원 시작점, 3차원 목적지 찾기
+    for j in range(n):
+        if grid[i][j] == 3:
+            for k in range(4):
+                nr = i + row[k]
+                nc = j + col[k]
+                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0:
+                    sr2, sc2 = nr, nc # 2차원 시작점
+                    # 이제 3차원 목적지 찾기
+                    eh3 = k  # k가 면 idx가 될거임.
+                    er3 = m - 1  # r은 무조건 2임.
+                    if k == 0:  # 동
+                        ec3 = m - 1 - (nr - startx)
+                    elif k == 1:  # 서
+                        ec3 = nr - startx
+                    elif k == 3:  # 북.
+                        ec3 = m - 1 - (nc - starty)
+                    elif k == 2:  # 남
+                        ec3 = nc - starty
+                    break
+
+# ---------------------- 시간 이상 3차원 배열 만들기 ----------------------
+time_attack = [[[] * n for i in range(n)] for i in range(n)]
+time_info = [list(map(int, input().split())) for i in range(time_attack_num)]
+for tr, tc, td, tv in time_info:
+    v = tv
+    time_attack[tr][tc].append(0)
+
+    for k in range(n):
+        ntr = tr + row[td]
+        ntc = tc + col[td]
+        if not (0 <= ntr < n and 0 <= ntc < n) or grid[ntr][ntc]:
+            break
+        time_attack[ntr][ntc].append(tv)
+        tv += v
+        tr = ntr
+        tc = ntc
+
+
+# --------------------------------- 함수 ---------------------------------
 def bfs3(sh, sr, sc, eh, er, ec):
     visited = [[[False] * n for i in range(n)] for i in range(5)]
     visited[sh][sr][sc] = True
@@ -191,20 +267,21 @@ def bfs3(sh, sr, sc, eh, er, ec):
             nr = r + row[k]
             nc = c + col[k]
             nh = h
+            # 실수하면 큰일난다.
             if h == 4:  # 윗면이면 어디로든지 갈 수 있음
-                if nr < 0:
+                if nr < 0:  # 북
                     nh = 3
                     nr = 0
                     nc = m - 1 - nc
-                elif nr >= m:
+                elif nr >= m:  # 남
                     nh = 2
                     nr = 0
                     nc = nc
-                elif nc < 0:
+                elif nc < 0:  # 서
                     nh = 1
                     nc = nr
                     nr = 0
-                elif nc >= m:
+                elif nc >= m:  # 동
                     nh = 0
                     nc = m - 1 - nr
                     nr = 0
@@ -267,6 +344,8 @@ def bfs3(sh, sr, sc, eh, er, ec):
             visited[nh][nr][nc] = True
             q.append((nh, nr, nc, time + 1, path + [(nh, nr, nc)]))
     return -1
+
+
 def bfs2(sr, sc, er, ec):
     visited = [[False] * n for i in range(n)]
     visited[sr][sc] = True
@@ -293,94 +372,19 @@ def bfs2(sr, sc, er, ec):
             q.append((nr, nc, time + 1))
     return -1
 
-from collections import deque
-
-n, m, time_attack_num = map(int, input().split())
-grid = [list(map(int, input().split())) for i in range(n)]
-cube = []
-for _ in range(5):
-    tmp = [list(map(int, input().split())) for i in range(m)]
-    cube.append(tmp)
-
-time_attack = [[[] * n for i in range(n)] for i in range(n)]
-time_info = [list(map(int, input().split())) for i in range(time_attack_num)]
-row = [0, 0, 1, -1]
-col = [1, -1, 0, 0]
-for tr, tc, td, tv in time_info:
-    v = tv
-    time_attack[tr][tc].append(0)
-
-    for k in range(n):
-        ntr = tr + row[td]
-        ntc = tc + col[td]
-        if not (0 <= ntr < n and 0 <= ntc < n) or grid[ntr][ntc]:
-            break
-        time_attack[ntr][ntc].append(tv)
-        tv += v
-        tr = ntr
-        tc = ntc
-
-sh3, sr3, sc3 = -1, -1, -1  # 3차원 시작점 o
-eh3, er3, ec3 = -1, -1, -1  # 3차원 탈출구
-sr2, sc2 = -1, -1,  # 2차원 시작점
-er2, ec2 = -1, -1  # 2차원 탈출구 o
-
-
-def find():
-    for i in range(n):
-        for j in range(n):
-            if grid[i][j] == 3:
-                return i, j
-
-
-startx, starty = find()
-
-for i in range(n):
-    for j in range(n):
-        if grid[i][j] == 4:
-            er2, ec2 = i, j
-
-for h in range(5):
-    for i in range(m):
-        for j in range(m):
-            if cube[h][i][j] == 2:
-                sh3, sr3, sc3 = h, i, j
-
-for i in range(n):
-    for j in range(n):
-        if grid[i][j] == 3:
-            for k in range(4):
-                nr = i + row[k]
-                nc = j + col[k]
-                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0:
-                    sr2, sc2 = nr, nc
-                    eh3 = k  # k가 면 idx가 될거임.
-                    er3 = m - 1  # r은 무조건 2임.
-                    if k == 0:  # 동쪽..
-                        ec3 = m - 1 - (nr - startx)
-
-                    elif k == 1:  # 서쪽..
-                        ec3 = nr - startx
-
-                    elif k == 3:  # 북쪽..
-                        ec3 = m - 1 - (nc - starty)
-                    elif k == 2:  # 남쪽..
-                        ec3 = nc - starty
-                    break
-
-
+# --------------------------------- 메인 ---------------------------------
 ans = bfs3(sh3, sr3, sc3, eh3, er3, ec3)
-if ans != -1:
-    ans += 1  # 2차원 평면으로 내려왔어
+if ans != -1: # ans == -1이면 3차원 탈출도 못함
+    ans += 1
 
-    for attack in time_attack[sr2][sc2]:
+    for attack in time_attack[sr2][sc2]: # 내려왔는데 시간이상에 바로 걸려버림
         if attack <= ans:
             ans = -1
             break
 
     if ans != -1:
         next_ans = bfs2(sr2, sc2, er2, ec2)
-        if next_ans == -1:
+        if next_ans == -1: # 2차원 탈출 못함
             ans = -1
         else:
             ans += next_ans
