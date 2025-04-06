3dbd50f735f3897e5f84325c010dd1c5b03575da
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-04-06 12:23:32 +0900

    Title: 싸움땅, Time: 94ms, Memory: 18MB, Status: Passed - Codetree

--- 250406/싸움땅/battle-ground.py
+++ 250406/싸움땅/battle-ground.py
@@ -25,7 +25,7 @@ player_lst = [0]
 player_grid = [[0] * n for i in range(n)]
 for _ in range(pm):
     r, c, d, s = map(int, input().split())
-    player_lst.append((r - 1, c - 1, d, s, 0))
+    player_lst.append([r - 1, c - 1, d, s, 0])
     player_grid[r - 1][c - 1] = _ + 1
 
 score = [0] * (pm + 1)
@@ -33,39 +33,36 @@ row = [-1, 0, 1, 0]
 col = [0, 1, 0, -1]
 
 
-def move(win_idx, lose_idx):
+def move(win_idx, lose_idx, x, y):
     # 진 플레이어 이동
-    lr, lc, ld, ls, lgun = player_lst[lose_idx]
-    if lgun:
-        gun_grid[lr][lc].append(lgun)
-        lgun = 0
-
+    _, _, d, s, gun = player_lst[lose_idx]
+    if gun:
+        gun_grid[x][y].append(gun)
+        gun = 0
     for k in range(4):
-        nlr = lr + row[ld]
-        nlc = lc + col[ld]
-        if 0 <= nlr < n and 0 <= nlc < n and player_grid[nlr][nlc] == 0:
+        nr = x + row[d]
+        nc = y + col[d]
+        if 0 <= nr < n and 0 <= nc < n and player_grid[nr][nc] == 0:
             break
         else:
-            ld = (ld + 1) % 4
-    nlr = lr + row[ld]
-    nlc = lc + col[ld]
-    if gun_grid[nlr][nlc]:
-        lgun = max(gun_grid[nlr][nlc])
-        gun_grid[nlr][nlc].remove(lgun)
-    player_lst[lose_idx] = (nlr, nlc, ld, ls, lgun)
-    player_grid[nlr][nlc] = lose_idx
+            d = (d + 1) % 4
+    if gun_grid[nr][nc]:
+        gun = max(gun_grid[nr][nc])
+        gun_grid[nr][nc].remove(gun)
+    player_lst[lose_idx] = [nr, nc, d, s, gun]
+    player_grid[nr][nc] = lose_idx
 
     # 이긴 플레이어 이동
-    wr, wc, wd, ws, wgun = player_lst[win_idx]
-    if gun_grid[wr][wc]:
-        other_gun = max(gun_grid[wr][wc])
-        if other_gun > wgun:
-            gun_grid[wr][wc].remove(other_gun)
+    _, _, d, s, gun = player_lst[win_idx]
+    if gun_grid[x][y]:
+        other_gun = max(gun_grid[x][y])
+        if other_gun > gun:
+            gun_grid[x][y].remove(other_gun)
             if gun:
-                gun_grid[wr][wc].append(wgun)
-            wgun = other_gun
-    player_lst[win_idx] = (wr, wc, wd, ws, wgun)
-    player_grid[wr][wc] = win_idx
+                gun_grid[x][y].append(gun)
+            gun = other_gun
+    player_lst[win_idx] = [x, y, d, s, gun]
+    player_grid[x][y] = win_idx
 
 
 for t in range(turn):
@@ -78,6 +75,7 @@ for t in range(turn):
         nr = r + row[d]
         nc = c + col[d]
         player_grid[r][c] = 0
+        player_lst[p][2] = d
         if not player_grid[nr][nc]:  # 빈 곳
             other_gun = 0
             if gun_grid[nr][nc]:
@@ -88,9 +86,8 @@ for t in range(turn):
                 gun_grid[nr][nc].remove(other_gun)
                 gun = other_gun
             player_grid[nr][nc] = p
-            player_lst[p] = (nr, nc, d, s, gun)
+            player_lst[p] = [nr, nc, d, s, gun]
         else:
-            player_lst[p] = (nr, nc, d, s, gun)
             px = player_grid[nr][nc]
             rx, cx, dx, sx, gunx = player_lst[px]  # 상대 정보
             win_idx = lose_idx = 0
@@ -106,7 +103,6 @@ for t in range(turn):
             else:
                 win_idx, lose_idx = px, p
             score[win_idx] += abs(my_power - other_power)
-            move(win_idx, lose_idx)
+            move(win_idx, lose_idx, nr, nc)
 
 print(*score[1:])
-
